02f8ed267d531ee39dc1a79cb2b96f4e9f4ced35
yyin <1398035515@qq.com>
2021-12-03 16:06:43 +0800

    feat
    add function of new\edit\delete
    add overview of individual information

--- src/main/java/com/example/javafxdemo/MainApp.java
+++ src/main/java/com/example/javafxdemo/MainApp.java
@@ -8,11 +8,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-;
+
 
 public class MainApp extends Application {
 
@@ -43,7 +44,7 @@ public class MainApp extends Application {
     /**
      * Returns the data as an observable list of Persons.
      *
-     * @return
+     * @return :  personData
      */
     public ObservableList<Person> getPersonData() {
         return personData;
@@ -103,10 +104,48 @@ public class MainApp extends Application {
     /**
      * Returns the main stage.
      *
-     * @return
+     * @return: primaryStage
      */
     public Stage getPrimaryStage() {
         return primaryStage;
+    }
+
+    /**
+     * Opens a dialog to edit details for the specified person. If the user
+     * clicks OK, the changes are saved into the provided person object and true
+     * is returned.
+     *
+     * @param person the person object to be edited
+     * @return true if the user clicked OK, false otherwise.
+     */
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Load the fxml file and create a new stage for the popup dialog.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Create the dialog Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Set the person into the controller.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Show the dialog and wait until the user closes it
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.javafxdemo {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires junit;
 
     opens com.example.javafxdemo to javafx.fxml;
     exports com.example.javafxdemo;
